6b2b8deb661733550661c1af1607a6a1ea510835
erzh <cmpan@qq.com>
2017-05-05 10:31:12 +0800

    Initial commit

---
+++ lib/Exception.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Windwork
+ * 
+ * 一个开源的PHP轻量级高效Web开发框架
+ * 
+ * @copyright Copyright (c) 2008-2017 Windwork Team. (http://www.windwork.org)
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace wf\route;
+
+/**
+ * Windwork路由异常类
+ * 
+ * @package     wf.route
+ * @author      cm <cmpan@qq.com>
+ * @since       0.1.0
+ */
+class Exception extends \Exception 
+{
+	
+}
+
